76716aa7bacd8ee62df77f5182672a8e5f1ca68e
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-08 21:40:50 -0500

    Hook up checkout page to Stripe and backend services. [#135470677]

--- Angular/angularjs/controllers/StripeController.js
+++ Angular/angularjs/controllers/StripeController.js
@@ -5,29 +5,50 @@
 
     ng.module("congo")
     .constant("baseUrl", "http://ec2-34-193-194-23.compute-1.amazonaws.com/Congo-Logic-Api/Api/")
-    .controller("stripeCtrl", ["$scope", "$http", "baseUrl", function($scope, $http, baseUrl) {
+    .controller("stripeCtrl", ["$scope", "$http", "$state", "baseUrl", function($scope, $http, $state, baseUrl) {
       var accountId = getCookie("AccountID");
 
       $scope.data = {
-        "stripe": {}
+        "stripe": {
+          "number": "4242424242424242",
+          "exp_month": "03",
+          "exp_year": "18",
+          "cvc": "123",
+          "address_zip": "12345"
+        }
       };
+
+      var form = document.getElementById("checkout-form");
+      var submitButton = form.getElementsByClassName("submit")[0];
+
+      var errorMessage = document.getElementById("payment-errors");
 
       function submitOrder(status, response) {
         if (response.error) {
-          $("form .payment-errors").html(response.error.message);
-          $("form .submit").prop("disabled", false);
+          ng.element(errorMessage).html(response.error.message);
+          ng.element(submitButton).prop("disabled", false);
+          ng.element(submitButton).removeClass("disabled");
         }
         else {
           var token = response.id;
           $http.post(baseUrl + "order", {
+            "CustomerID": accountId,
             "AccountID": accountId,
             "Token": token
+          }).then(function(response) {
+            console.log(response.data);
+            ng.element(errorMessage).html("Your order has been submitted!");
+            $state.go("home");
+          }, function(response) {
+            console.log(response.data);
+            ng.element(errorMessage).html(response.data.message);
           });
         }
       }
 
       $scope.checkOut = function() {
-        $("form .submit").prop("disabled", true);
+        ng.element(submitButton).prop("disabled", true);
+        ng.element(submitButton).addClass("disabled");
         Stripe.card.createToken($scope.data.stripe, submitOrder);
       };
     }]);
